c537487524b607d2d637d14714fdea02a593d714
easy00 <leejy930924@gmail.com>
2016-12-17 18:35:53 +0900

    로그인 기능 추가하기

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Neurame1::Application.routes.draw do
   get "users/signup"
   post "users/signup_complete"
   get "users/login"
-  get "users/login_complete"
+  post "users/login_complete"
   get "users/logout_complete"
   root "gallery#posts"
   get "/:category" => 'gallery#posts_category'
